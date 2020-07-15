abc3d8e4b5822b5ddbeb81796c7ae31aff1d68cf
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2020-07-15 21:39:40 +0700

    Add button styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
 
 import Layout from '../components/layout'
 import '../styles/main.scss'
@@ -15,9 +12,17 @@ const IndexPage = () => (
       <h2>Hi there!</h2>
       <h1>I'm Fahmi.</h1>
       <p>I'm a software engineer who places his heart on arts. I do illustrations, user interfaces, as well as building apps.</p>
-      <FontAwesomeIcon icon={['fab', 'apple']} />
-      <FontAwesomeIcon icon={['fab', 'microsoft']} />
-      <FontAwesomeIcon icon={['fab', 'instagram']} />
+      <div className="header-btn">
+        <button className="btn-github">
+          <FontAwesomeIcon icon={['fab', 'github']} />
+        </button>
+        <button className="btn-instagram">
+          <FontAwesomeIcon icon={['fab', 'instagram']} />
+        </button>
+        <button className="btn-twitter">
+          <FontAwesomeIcon icon={['fab', 'twitter']} />
+        </button>
+      </div>
     </div>
   </Layout>
 )
