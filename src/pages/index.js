cad82dc650c820cbb9abc813826d4abe7413662c
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2020-07-16 20:05:03 +0700

    Add header pic layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,19 +9,28 @@ import '../styles/main.scss'
 const IndexPage = () => (
   <Layout>
     <div className="header">
-      <h2>Hi there!</h2>
-      <h1>I'm Fahmi.</h1>
-      <p>I'm a software engineer who places his heart on arts. I do illustrations, user interfaces, as well as building apps.</p>
-      <div className="header-btn">
-        <button className="btn-github">
-          <FontAwesomeIcon icon={['fab', 'github']} />
-        </button>
-        <button className="btn-instagram">
-          <FontAwesomeIcon icon={['fab', 'instagram']} />
-        </button>
-        <button className="btn-twitter">
-          <FontAwesomeIcon icon={['fab', 'twitter']} />
-        </button>
+      <div className="header-pics">
+        <div className="header-pic-long"></div>
+        <div className="header-pic-long">
+          <div className="header-pic-square"></div>
+          <div className="header-pic-square"></div>
+        </div>
+      </div>
+      <div>
+        <h2>Hi there!</h2>
+        <h1>I'm Fahmi.</h1>
+        <p>I'm a software engineer who places his heart on arts. I do illustrations, user interfaces, as well as building apps.</p>
+        <div className="header-btn">
+          <button className="btn-github">
+            <FontAwesomeIcon icon={['fab', 'github']} />
+          </button>
+          <button className="btn-instagram">
+            <FontAwesomeIcon icon={['fab', 'instagram']} />
+          </button>
+          <button className="btn-twitter">
+            <FontAwesomeIcon icon={['fab', 'twitter']} />
+          </button>
+        </div>
       </div>
     </div>
   </Layout>
